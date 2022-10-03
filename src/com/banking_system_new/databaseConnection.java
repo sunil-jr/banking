867d50b318b29b0d097981945aa22bd4d9e19cd0
unknown <subedisunil495@gmail.com>
2022-10-03 17:58:40 +0545

    update on database connection

--- src/com/banking_system_new/databaseConnection.java
+++ src/com/banking_system_new/databaseConnection.java
@@ -1,11 +1,11 @@
 package com.banking_system_new;
+
 import java.sql.*;
 
 public class databaseConnection {
-public Connection getConnection() throws Exception
-    {
+    public Connection getConnection() throws Exception {
         Class.forName("com.mysql.cj.jdbc.Driver");
-        Connection connection=DriverManager.getConnection("jdbc:mysql://localhost:3306/bank","root","sunil@#$%$#@");
+        Connection connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/bank", "root", "sunil@#$%$#@");
         return connection;
     }
 }
